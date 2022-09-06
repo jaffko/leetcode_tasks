189c8096bf4f6ea0d783d9b8bae86e7b8ba9298b
Ildar <steel14@yandex.ru>
2022-09-06 22:00:19 +0500

    added binary search

---
+++ Binary_search/Program.cs
@@ -0,0 +1,31 @@
+﻿int SearchBin(int[] nums, int target)
+{
+    int i = nums.Length / 2;
+    if (nums[i] == target) return i;
+    else if (nums[i] > target)
+    {
+        while (i >= 0)
+        {
+            if (target == nums[i]) return i;
+            i--;
+        }
+        return -1;
+    }
+    else if (nums[i] < target)
+    {
+        while (i < nums.Length)
+        {
+            if (target == nums[i]) 
+            {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+    return -1;    
+}
+
+int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+int target = 12;
+System.Console.WriteLine(SearchBin(nums, target));
